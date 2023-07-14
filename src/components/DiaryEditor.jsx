b545a068a9530030bb368ba5e44c6110c868b40c
AhnEunJin <eunjin.an@datastreams.co.kr>
2023-07-14 16:11:52 +0900

    select, save button + CSS styling

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -4,12 +4,19 @@ export default function DiaryEditor() {
   const [state, setState] = useState({
     author: '',
     content: '',
+    emotion: 1,
   });
-  const { author, content } = state;
+  const { author, content, emotion } = state;
 
   const handleChangeState = (e) => {
     const { name, value } = e.target;
     setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('SUCCESS');
+    setState({ author: '', content: '', emotion: 1 });
   };
 
   return (
@@ -27,10 +34,21 @@ export default function DiaryEditor() {
         <textarea
           name='content'
           cols='30'
-          rows='10'
           value={content}
           onChange={handleChangeState}
         ></textarea>
+      </div>
+      <div>
+        <select name='emotion' value={emotion} onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
