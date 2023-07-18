65f0d43d2d25ba9b91c56d6114c6738062bcd149
AhnEunJin <eunjin.an@datastreams.co.kr>
2023-07-18 10:24:22 +0900

    일기분석 메모이제이션

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './components/DiaryEditor';
 import DiaryList from './components/DiaryList';
@@ -11,7 +11,6 @@ function App() {
     const res = await fetch(
       'https://jsonplaceholder.typicode.com/comments'
     ).then((res) => res.json());
-    console.log(res);
 
     const initData = res.slice(0, 20).map((item) => {
       return {
@@ -56,9 +55,25 @@ function App() {
     );
   };
 
+  const getDiaryAnalsis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((item) => item.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalsis;
+
   return (
     <div>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
